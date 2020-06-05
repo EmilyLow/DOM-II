1e055721ce04b50d068e2d02af45bb8485133f12
ErinFlight <erinflight@gmail.com>
2020-06-04 20:37:58 -0500

    Added dblclick event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+//mouseover
+
+
+//keydown (any key pressed)
+
+
+//dblclick
+//Flip background and text
+
+let whole = document.getElementsByTagName('body');
+
+whole[0].addEventListener('dblclick', (event) => {
+    whole[0].style.color = "white";
+    whole[0].style.backgroundColor = "black";
+});
+
+console.log("test");
