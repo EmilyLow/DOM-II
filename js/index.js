1056fadbf45997ce2aac876d896dff69a84b9166
ErinFlight <erinflight@gmail.com>
2020-06-05 19:20:19 -0500

    Added all listeners, preventing propogation, and prevented default for clicking nav items

--- js/index.js
+++ js/index.js
@@ -17,6 +17,31 @@ whole[0].addEventListener('keydown', (event) => {
     centerPics[1].setAttribute('src', "img/adventure.jpg");
 });
 
+//keyup
+//Change text of first paragraph if key is released
+let firstText = document.querySelector(".intro p");
+
+document.addEventListener('keyup', (event) => {
+    firstText.textContent = "Do not go gentle into that good night, Old age should burn and rave at close of day; Rage, rage against the dying of the light. Though wise men at their end know dark is right, Because their words had forked no lightning they Do not go gentle into that good night."
+});
+
+
+
+//copy
+//Change h2 text if user copies h2
+
+let funBus = document.querySelector(".intro h2");
+
+funBus.addEventListener('copy', (event) => 
+{
+    funBus.textContent = "Welcome To The Doom Bus!";}
+    );
+
+//load
+//Console logs that the page has loaded
+window.addEventListener('load', (event) => {
+    console.log("Page loaded.");
+});
 
 //dblclick
 //Flip background and text colors
@@ -25,6 +50,52 @@ whole[0].addEventListener('keydown', (event) => {
 whole[0].addEventListener('dblclick', (event) => {
     whole[0].style.color = "white";
     whole[0].style.backgroundColor = "black";
+    
 });
 
-console.log("test");
+//focus
+//On focusing, the background of the form turns blue
+let form = document.querySelector('input[type="text"]');
+let fillerText = document.querySelector(".form input")
+form.addEventListener('focus', (event) => {
+    event.target.style.background = "lightblue";
+});
+
+//select
+//Text has been entered and then selected inside the form
+let input = document.querySelector('input');
+input.addEventListener('select', (event) => {
+    event.target.style.background = "red";
+} );
+
+//mouseenter
+//The mouse enters the bottom image, and the image zooms in
+let finalImage = document.querySelector(".content-destination img");
+finalImage.addEventListener ('mouseenter', (event) => {
+ 
+    finalImage.style.transform = "scale(2)";
+    finalImage.style.transition = "transform 1s";
+});
+
+//mouseleave
+//The mouse leaves the bottom image, and the image zooms out
+finalImage.addEventListener ('mouseleave', (event) => {
+    finalImage.style.transform = "scale(1)";
+});
+
+//dblclick nest
+//Creating a doubleclick event on first paragraph and preventing propogation to full page doubleclick
+firstText.addEventListener('dblclick', (event) => {
+    firstText.style.color = "orange";
+    firstText.style.backgroundColor = "grey";
+    event.stopPropagation();
+});
+
+//preventdefault
+let navItems = document.querySelectorAll('a');
+
+navItems.forEach((item) => item.addEventListener('click', (event) => {
+    item.textContent = "clicked";
+    event.preventDefault();
+} 
+    ));
